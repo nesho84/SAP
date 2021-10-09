83815c996dcdd8be0311c88325a7b6784334d722
nesho84 <ademi.neshat@gmail.com>
2021-10-10 01:14:49 +0200

    Fixed Home active

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
-session_start();
+$url = "home";
 
-$active = filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL);
+if (isset($_GET['url'])) {
+  $url = filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL);
+}
+
+$active = $url;
 
 include "./includes/header.php";
 ?>
@@ -9,8 +13,7 @@ include "./includes/header.php";
 <main id="content" class="px-3">
   <?php
   // Dinamic Content here
-  if (isset($_GET['url'])) {
-    $url = filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL);
+  if ($url !== "home") {
     include "./pages/" . $url . ".php";
   } else {
     include "./pages/home.php";
